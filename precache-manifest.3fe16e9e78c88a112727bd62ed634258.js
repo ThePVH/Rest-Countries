e2898a545e5a19edcad09441a51b2016d8a8f7ad
Tomi <viethung.pham93@gmail.com>
2020-04-14 10:06:15 +0200

    Updates

---
+++ precache-manifest.3fe16e9e78c88a112727bd62ed634258.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0be7479680255b7e2cd92d8f9313a51c",
+    "url": "/Rest-Countries/index.html"
+  },
+  {
+    "revision": "40ff38bbdd5b21a1139c",
+    "url": "/Rest-Countries/static/css/main.5d2bdb65.chunk.css"
+  },
+  {
+    "revision": "ee95c85574d7bce0e559",
+    "url": "/Rest-Countries/static/js/2.840223fc.chunk.js"
+  },
+  {
+    "revision": "5446a3b28fb6ecbe636e29e1e75ce1f4",
+    "url": "/Rest-Countries/static/js/2.840223fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40ff38bbdd5b21a1139c",
+    "url": "/Rest-Countries/static/js/main.c9a37b56.chunk.js"
+  },
+  {
+    "revision": "35b6e53929020c79a41a",
+    "url": "/Rest-Countries/static/js/runtime-main.720c6742.js"
+  }
+]);
